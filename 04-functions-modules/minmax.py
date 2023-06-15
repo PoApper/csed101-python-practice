1a3e330e4d55a8b69e8416545c351bbb88354c17
HyoJeong Yun <hodori@postech.ac.kr>
2023-06-15 13:51:20 +0900

    min max test (#1)
    * implt minmax test
    
    * fix return
    
    * feedback

---
+++ 04-functions-modules/minmax.py
@@ -0,0 +1,31 @@
+import random
+
+def myMin(iterable):
+    """
+    >>> myMin([7, 6, 5])
+    5
+    >>> myMin([3, 2, 5, 7, 8, 90, 5])
+    2
+    >>> sample = [i for i in range(100)]; \
+        random.shuffle(sample); \
+        myMin(sample)
+    0
+    """
+    # TODO
+
+def myMax(iterable):
+    """
+    >>> myMax([7, 6, 5])
+    7
+    >>> myMax([3, 2, 5, 7, 8, 90, 5])
+    90
+    >>> sample = [i for i in range(100)]; \
+        random.shuffle(sample); \
+        myMax(sample)
+    99
+    """
+    # TODO
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod(verbose=True)
