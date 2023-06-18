db56335912f9631b0dbf8961124e2a3e02d890fc
Ryang Sohn <loop.infinitely@gmail.com>
2023-06-18 15:33:38 +0900

    feat(04): add an exercise, "Swap Index"

--- 04-functions-modules/guide.md
+++ 04-functions-modules/guide.md
@@ -1,8 +1,17 @@
 # functions and modules
 
 ## MinMax
+
 - difficulty: ★☆☆
 - time: ~ 15m for beginners
 - background: list, iteration
 
 Implement `min`, `max` functions on your own.
+
+## Swap Index
+
+- difficulty: ★☆☆
+- time: ~ 15m for beginners
+- background: list
+
+Implement a function which swaps indices of list.
---
+++ 04-functions-modules/swap_index-answer.py
@@ -0,0 +1,19 @@
+def swap_indices(ls, i, j):
+    """
+    Swap ls[i] and ls[j].
+
+    >>> ls = [0, 1, 2, 3]
+    >>> swap_indices(ls, 0, 2)
+    >>> ls
+    [2, 1, 0, 3]
+    >>> swap_indices(ls, 1, 2)
+    >>> ls
+    [2, 0, 1, 3]
+    """
+    ls[i], ls[j] = ls[j], ls[i]
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(verbose=True)
