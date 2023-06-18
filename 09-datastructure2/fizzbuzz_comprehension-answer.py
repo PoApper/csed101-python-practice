ae9c735225758e75ba61c978e041b4298a44887f
Ryang Sohn <loop.infinitely@gmail.com>
2023-06-18 16:13:35 +0900

    test(09): add test case for fizzbuzz comprehension
    We have to test if "fizzbuzz" is printed properly.

--- 09-datastructure2/fizzbuzz_comprehension-answer.py
+++ 09-datastructure2/fizzbuzz_comprehension-answer.py
@@ -15,6 +15,8 @@ def fizzbuzz(n):
     [1, 2, 'fizz']
     >>> fizzbuzz(10)
     [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz']
+    >>> fizzbuzz(20)
+    [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizzbuzz', 16, 17, 'fizz', 19, 'buzz']
     """
 
     def fizzbuzz_single(x):
