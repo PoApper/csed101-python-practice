c9342f8b04659a8796f1ccd8e9e40844b4e1d7d2
Ryang Sohn <loop.infinitely@gmail.com>
2023-06-19 22:27:38 +0900

    feat(07): add an exercise, "Palindrome"

---
+++ 07-datastructure1/palindrome-answer.py
@@ -0,0 +1,24 @@
+import copy
+
+
+def palindrome_check(ls):
+    """
+    Check if `ls` is a palindrome or not. Return `True` if ls is same when reversed,
+    otherwise return `False`.
+
+    >>> palindrome_check([0, 1, 2])
+    False
+    >>> palindrome_check([0, 1, 0])
+    True
+    >>> palindrome_check([1, 2, 3, 2, 1])
+    True
+    """
+    reversed_list = copy.copy(ls)
+    reversed_list.reverse()
+    return ls == reversed_list
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(verbose=True)
