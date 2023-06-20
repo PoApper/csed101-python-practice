86741d3c4e21182c34eff7adc80b265ffc1e6c60
Ryang Sohn <loop.infinitely@gmail.com>
2023-06-20 17:57:46 +0900

    New exercise for lists: "Palindrome" (#11)
    * feat(07): add an exercise, "Palindrome"
    
    * feat(07): add additional answer

--- 07-datastructure1/guide.md
+++ 07-datastructure1/guide.md
@@ -1,1 +1,10 @@
 # data structure 1 | list, tuple, dictionary
+
+## Palindrome
+
+- difficulty: ★☆☆
+- time: ~ 15m for beginners
+- background: lists
+
+Check if the given list is palindrome.
+
---
+++ 07-datastructure1/palindrome.py
@@ -0,0 +1,21 @@
+import copy
+
+
+def palindrome_check(ls):
+    """
+    Check if `ls` is a palindrome or not. Return `True` if ls is same when reversed,
+    otherwise return `False`.
+
+    >>> palindrome_check([0, 1, 2])
+    False
+    >>> palindrome_check([0, 1, 0])
+    True
+    >>> palindrome_check([1, 2, 3, 2, 1])
+    True
+    """
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(verbose=True)
