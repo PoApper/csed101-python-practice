ec921153e2b48634d9cc7654aa924202aeff9094
Ryang Sohn <loop.infinitely@gmail.com>
2023-06-25 16:32:38 +0900

    feat(09): add an exercise, "Sum Calculator"

--- 09-datastructure2/readme.md
+++ 09-datastructure2/readme.md
@@ -7,3 +7,11 @@
 - background: list-comprehension
 
 Solve the fizzbuzz problem in lecture 6 using list comprehension.
+
+## Sum Calculator
+
+- difficulty: ★★☆
+- time: ~ 30m for beginners
+- background: loops, dictionaries, variable-length-args, keyword-args
+
+Implement a function `sum_args`, which sums the numbers provided as arguments.
---
+++ 09-datastructure2/sum_calculator.py
@@ -0,0 +1,19 @@
+def sum_args(*args, **kwargs):
+    """
+    Calculate the sum of the numbers given as arguments. If there are string in
+    the arguments, look it up from `kwargs` and use the value corresponding to the
+    name provided as string.
+
+    >>> sum_args(1, 2, 3)
+    6
+    >>> sum_args(1, 2, "a", "b", a=1, b=3)
+    7
+    >>> sum_args("x", "y", x=1, y=2)
+    3
+    """
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(verbose=True)
