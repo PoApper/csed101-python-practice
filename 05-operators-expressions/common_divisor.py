33dfffb83aa22d4a6a0158a07b351ea9c2fd4001
Ryang Sohn <loop.infinitely@gmail.com>
2023-07-02 09:03:38 +0900

    feat(05): add exercise, "Common Divisor" (#15)

---
+++ 05-operators-expressions/common_divisor.py
@@ -0,0 +1,15 @@
+def common_divisor_check(a, b, g):
+    """
+    Return True if `g` is a common divisor of `a` and `b` and False otherwise.
+
+    >>> common_divisor_check(4, 6, 2)
+    True
+    >>> common_divisor_check(7, 5, 2)
+    False
+    """
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod(verbose=True)
