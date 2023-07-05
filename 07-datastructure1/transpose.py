7d76558e5915621e0529fb2afcdbdbb9b8971856
Ryang Sohn <loop.infinitely@gmail.com>
2023-07-05 13:34:03 +0900

    docs(07): add clarification regarding input

--- 07-datastructure1/transpose.py
+++ 07-datastructure1/transpose.py
@@ -4,6 +4,7 @@ def transpose(mat):
     Given a 2D list M consisting of R 1D lists whose sizes are C, a transpose
     of M is a 2D list T, consisting of C 1D lists whose sizes are R. T should
     satisfy M[i][j] = T[j][i], where 0 <= i < R and 0 <= j < C.
+    You can assume that all lists in `mat` have same sizes.
 
     >>> transpose([[1, 2, 3], [4, 5, 6]])
     [[1, 4], [2, 5], [3, 6]]
